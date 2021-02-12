a107474bf78e257d70e141298083dfd3454b3d5b
Toai Vo <toaivo@debtest2.resourceonline.org>
2021-02-11 21:29:46 -0800

    Intitial Version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sp-gen
 Simple perl script to generate sp-key.pem and sp-cert.pem to use with SAML
+
+*  You need to put these two scripts in your cgi-bin.
+*  Enter https://yoursite/cgi-bin/sp-gen.pl?SITENAME=yoursite.
+*  For example https://janedoe.net/cgi-bin/sp-gen.pl?SITENAME=mywebsite.janedone.net
+
---
+++ sp-gen.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+#
+#  PROGRAM:  sp-gen.pl
+#
+#  PURPOSE:  A simple Perl to generate sp-key.pem and sp-cert.pem for SAML
+#  VERSION:  1.0
+#  Created by toai vo toaivo@stanford.edu
+#
+use strict;
+use warnings;
+use CGI qw(:standard);
+
+my $query = new CGI;
+my $SITEURL = $query->param('SITENAME');
+
+my $CLIENTIP = $ENV{'REMOTE_ADDR'};
+my $CGIDIR = '/home/stanford/public_html/cgi-bin';
+
+print "Content-type: text/html\n\n";
+print "<pre>\n\n";
+my @cmd   = ("$CGIDIR/sp-gen.sh", "-h", $SITEURL );
+system (@cmd);
+print "</pre>\n\n";
